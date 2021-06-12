989a836fada4f66a8a10a24b9f4dc04e3a8ba1c7
Kazi Nizamul <kazinizamul.work@hotmail.com>
2021-06-12 16:55:33 +0530

    commit

---
+++ src/app/App.jsx
@@ -0,0 +1,21 @@
+/* ------------------------------ react things ------------------------------ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+/* ------------------------------- local deps ------------------------------- */
+import './App.css';
+/* ------------------------------- components ------------------------------- */
+import Header from '../components/Header'
+import Home from '../components/Home';
+
+
+function App() { 
+  return (
+    <div className="App">
+      <Header />
+      <Home/>
+    </div>
+  );
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
